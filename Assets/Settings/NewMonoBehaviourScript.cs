1c64ba49f2b00f1e861d76a1e6922ab54a196db4
khoido2003 <khoizpro31@gmail.com>
2025-05-06 17:40:57 +0700

    First commit

---
+++ Assets/Settings/NewMonoBehaviourScript.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (transform == transform)
+        {
+            Update();
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    // Do something
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update() { }
+}
